e17cc6db1ea447906033d32a4014e4051558123a
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2022-04-07 16:40:32 +0200

    Initial commit

---
+++ rpi-question-generator.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Rpi Question Generator
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: reintanz
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
